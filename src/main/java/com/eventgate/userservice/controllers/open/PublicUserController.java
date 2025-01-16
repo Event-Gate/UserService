1ebdfd0470d568cd23fa871001f3d3f9db242a75
apostorial <t.elouadrassi@gmail.com>
2025-01-16 06:33:26 +0100

    Modified the types and methods to match MongoDB.

--- src/main/java/com/eventgate/userservice/controllers/open/PublicUserController.java
+++ src/main/java/com/eventgate/userservice/controllers/open/PublicUserController.java
@@ -1,6 +1,7 @@
 package com.eventgate.userservice.controllers.open;
 
 import com.eventgate.userservice.dtos.DataUserResponse;
+import com.eventgate.userservice.exceptions.EntityNotFoundException;
 import com.eventgate.userservice.services.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,24 +9,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
-import java.util.UUID;
-
 @RestController @RequiredArgsConstructor @RequestMapping("/api/public/users")
 public class PublicUserController {
     private final UserService userService;
 
     @GetMapping("/{userId}/followings")
-    public ResponseEntity<Set<UUID>> getFollowings(@PathVariable("userId") UUID userId) {
+    public ResponseEntity<Set<String>> getFollowings(@PathVariable("userId") String userId) throws EntityNotFoundException {
         return new ResponseEntity<>(userService.getFollowings(userId), HttpStatus.OK);
     }
 
     @GetMapping("/{sellerId}/followers")
-    public ResponseEntity<Set<String>> getFollowers(@PathVariable("sellerId") UUID sellerId) {
+    public ResponseEntity<Set<String>> getFollowers(@PathVariable("sellerId") String sellerId) throws EntityNotFoundException {
         return new ResponseEntity<>(userService.getFollowers(sellerId), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<DataUserResponse> getUser(@PathVariable("userId") UUID userId) {
+    public ResponseEntity<DataUserResponse> getUser(@PathVariable("userId") String userId) throws EntityNotFoundException {
         return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
     }
 }
--- src/main/java/com/eventgate/userservice/repositories/UserRepository.java
+++ src/main/java/com/eventgate/userservice/repositories/UserRepository.java
@@ -1,21 +1,29 @@
 package com.eventgate.userservice.repositories;
 
 import com.eventgate.userservice.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
+    @Aggregation(pipeline = {
+            "{ $match: { '_id': ?0 } }",
+            "{ $project: { '_id': 0, 'followerIds': 1 } }",
+            "{ $unwind: '$followerIds' }",
+            "{ $group: { '_id': null, 'ids': { $addToSet: '$followerIds' } } }"
+    })
+    Set<String> findFollowerIdsById(String userId);
 
-    @Query("SELECT u FROM User u JOIN u.followings f WHERE f.id = :userId")
-    Set<User> findFollowersById(@Param("userId") UUID userId);
-    @Query("SELECT f FROM User u JOIN u.followings f WHERE u.id = :userId")
-    Set<User> findFollowingsById(@Param("userId") UUID userId);
+    @Aggregation(pipeline = {
+            "{ $match: { '_id': ?0 } }",
+            "{ $project: { '_id': 0, 'followingIds': 1 } }",
+            "{ $unwind: '$followingIds' }",
+            "{ $group: { '_id': null, 'ids': { $addToSet: '$followingIds' } } }"
+    })
+    Set<String> findFollowingIdsById(String userId);
 }
