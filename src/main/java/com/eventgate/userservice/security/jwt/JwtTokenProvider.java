de6bce8ad8c86781ab9d68c84cb64f7074ef5188
apostorial <t.elouadrassi@gmail.com>
2025-01-16 06:32:54 +0100

    Removed the JWT generator method and replaced it with a call from the properties.

--- src/main/java/com/eventgate/userservice/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/eventgate/userservice/security/jwt/JwtTokenProvider.java
@@ -3,29 +3,28 @@ package com.eventgate.userservice.security.jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
-import java.security.SecureRandom;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.Date;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenProvider {
-    private final String jwtSecret = generateSecretKey();
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
-    public String generateToken(UUID customerId, String email, Collection<? extends GrantedAuthority> authorities) {
+    public String generateToken(String customerId, String email, Collection<? extends GrantedAuthority> authorities) {
         Date currentDate = new Date();
         long jwtExpiration = 86400000;
         Date expireDate = new Date(currentDate.getTime() + jwtExpiration);
 
         return Jwts.builder()
-                .subject(customerId.toString())
+                .subject(customerId)
                 .claim("email", email)
                 .claim("authorities", authorities.stream()
                         .map(GrantedAuthority::getAuthority)
@@ -38,14 +37,6 @@ public class JwtTokenProvider {
 
     private Key key(){
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
-    }
-
-    public String generateSecretKey() {
-        int length = 32;
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] keyBytes = new byte[length];
-        secureRandom.nextBytes(keyBytes);
-        return Base64.getEncoder().encodeToString(keyBytes);
     }
 
     public String getCustomerId(String token){
