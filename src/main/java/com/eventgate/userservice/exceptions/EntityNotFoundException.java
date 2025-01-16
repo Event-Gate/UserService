7d57e3e87f944a4c466193932ca3ceb117dc5c08
apostorial <t.elouadrassi@gmail.com>
2025-01-16 06:28:52 +0100

    Added a custom Entity Not Found Exception.

---
+++ src/main/java/com/eventgate/userservice/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.eventgate.userservice.exceptions;
+
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
